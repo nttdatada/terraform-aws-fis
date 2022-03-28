b6cae719f032deb794ba5933abd60963c49a43fa
Marcos Cianci <marcos.lopescianci@nttdata.com>
2022-03-28 16:33:35 -0300

    Update module fis

--- main.tf
+++ main.tf
@@ -7,14 +7,38 @@
 
 resource "local_file" "network-latency" {
 
-  filename          = "${path.module}/network-latency.json" 
+  filename          = "network-latency.json" 
   file_permission   = "0600"
 
-  content           = templatefile("${path.module}/templates/network-latency.tpl", {
+  content           = templatefile("templates/network-latency.tpl", {
     asg             = [var.target_ec2]
     region          = var.aws_region
     alarm           = var.alarm
     role            = var.role_arn
   })
 
+}
+
+resource "local_file" "create-templates" {
+  content = join("\n", [
+    "#!/bin/bash -ex",
+    "OUTPUT='.fis_cli_result'",
+    "TEMPLATES=( 'network-latency.json' )",
+    "for template in $${TEMPLATES[@]}; do",
+    "  aws fis create-experiment-template --cli-input-json file://$${template} --output text --query 'experimentTemplate.id' 2>&1 | tee -a $${OUTPUT}",
+    "done",
+    ]
+  )
+  filename        = "fis-create-experiment-templates.sh"
+  file_permission = "0600"
+}
+
+resource "null_resource" "create-templates" {
+  depends_on = [
+    local_file.network-latency
+  ]
+  provisioner "local-exec" {
+    when    = create
+    command = "bash fis-create-experiment-templates.sh"
+  }
 }
